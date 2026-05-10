54a2e74318f1507132aeccdef1756339d453d4cc
bayudeveloper <anugrahabayu4@gmail.com>
2026-05-10 15:30:55 +0700

    Upload config/index.js via Himmel Bot

---
+++ config/index.js
@@ -0,0 +1,12 @@
+export const config = {
+  botName: 'Lumi Hevia',
+  version: 'Alpha',
+  author: 'Bayu Official',
+  ownerNumber: ['62895406178006'],
+  timezone: 'Asia/Jakarta',
+  cooldown: 3000,
+  sessionPath: './session',
+  commandsPath: './commands',
+  himmelApi: 'https://www.himmel-api.my.id',
+  baseby: 'Bayu Official',
+}
